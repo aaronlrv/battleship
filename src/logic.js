ad145aa9494b0a9708cba949cdddadc471f7ae2a
aaronlrv <aaronvitharanage@gmail.com>
2022-07-13 12:44:11 +0400

    Add rough ship function

---
+++ src/logic.js
@@ -0,0 +1,17 @@
+function ships(name, coordinates) {
+  return {
+    name: name,
+    coordinates: [coordinates],
+    length() {
+      console.log(coordinates);
+      ships.coordinates.forEach((element) => {
+        let lengthValue = 0;
+        length++;
+        length: lengthValue;
+        console.log(length);
+      });
+    },
+  };
+}
+
+export { ships };
