ebc0f6f775156a1c29ba7ff4fa58bd5f1cc135e5
aaronlrv <aaronvitharanage@gmail.com>
2022-07-14 22:17:00 +0400

    Modify logic test to 2d array layout

--- src/logic.test.js
+++ src/logic.test.js
@@ -2,8 +2,8 @@ let logic = require("./logic");
 
 test("test sink function", () => {
   let ship1 = new logic.ships(
-    "Patrol",
-    [logic.field[0], logic.field[1], logic.field[2]],
+    "p",
+    [logic.field[1][1], logic.field[1][2], logic.field[1][3]],
     3
   );
   expect(ship1.sunk()).toBe("ship has sunk");
@@ -11,20 +11,10 @@ test("test sink function", () => {
 
 test("test hit function", () => {
   let ship1 = new logic.ships(
-    "Patrol",
-    [logic.field[0], logic.field[1], logic.field[2]],
+    "p",
+    [logic.field[1][1], logic.field[1][2], logic.field[1][3]],
     3
   );
 
   expect(ship1.hit()).toBe(2);
-});
-
-test("test hit function if length is 0", () => {
-  let ship1 = new logic.ships(
-    "Patrol",
-    [logic.field[0], logic.field[1], logic.field[2]],
-    3
-  );
-
-  expect(ship1.hit()).toBe("ship has already been sunk");
 });
