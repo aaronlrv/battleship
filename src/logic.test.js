5cc944daee49cb794b6d4160edb754182a87cf1d
aaronlrv <aaronvitharanage@gmail.com>
2022-07-17 11:30:33 +0400

    Fix horizontal test to adapt to new gameboard class

--- src/logic.test.js
+++ src/logic.test.js
@@ -17,25 +17,6 @@ test("test hit function", () => {
   expect(ship1.hit()).toBe(2);
 });
 
-test("test gameboard function horizontal", () => {
-  let gameboard = new logic.gameboard(field[1][1], "poo", 3, "horizontal");
-  gameboard.gameboardPosition();
-
-  expect(field).toStrictEqual([
-    ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10"],
-    ["b1", "poo", "poo", "poo", "b5", "b6", "b7", "b8", "b9", "b10"],
-    ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10"],
-    ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10"],
-    ["d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10"],
-    ["e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9", "e10"],
-    ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10"],
-    ["g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9", "g10"],
-    ["h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10"],
-    ["i1", "i2", "i3", "i4", "i5", "i6", "i7", "i8", "i9", "i10"],
-    ["j1", "j2", "j3", "j4", "j5", "j6", "j7", "j8", "j9", "j10"],
-  ]);
-});
-
 test("test gameboard function vertical", () => {
   let ship = new logic.ships("ship", logic.field[1][1], 3, "vertical");
   let variable = ship;
@@ -99,6 +80,55 @@ test("test gameboard function vertical", () => {
       "c9",
       "c10",
     ],
+    ["d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10"],
+    ["e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9", "e10"],
+    ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10"],
+    ["g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9", "g10"],
+    ["h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10"],
+    ["i1", "i2", "i3", "i4", "i5", "i6", "i7", "i8", "i9", "i10"],
+    ["j1", "j2", "j3", "j4", "j5", "j6", "j7", "j8", "j9", "j10"],
+  ]);
+});
+
+test("test gameboard function horizontal", () => {
+  let ship2 = new logic.ships("ship2", logic.field[1][1], 3, "horizontal");
+  let gameboard = new logic.gameboard();
+  gameboard.gameboardPosition(ship2, gameboard);
+
+  expect(gameboard.gameboardPosition(ship2, gameboard)).toEqual([
+    ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10"],
+    [
+      "b1",
+      {
+        name: "ship2",
+        start: "b2",
+        length: 3,
+        hp: 3,
+        orientation: "horizontal",
+      },
+      {
+        name: "ship2",
+        start: "b2",
+        length: 3,
+        hp: 3,
+        orientation: "horizontal",
+      },
+      {
+        name: "ship2",
+        start: "b2",
+        length: 3,
+        hp: 3,
+        orientation: "horizontal",
+      },
+      "b5",
+      "b6",
+      "b7",
+      "b8",
+      "b9",
+      "b10",
+    ],
+    ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10"],
+    ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10"],
     ["d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10"],
     ["e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9", "e10"],
     ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10"],
