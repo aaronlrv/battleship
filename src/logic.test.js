2f3b63f6314dc2a20d5a1b946ec4c64d2f6ae518
aaronlrv <aaronvitharanage@gmail.com>
2022-07-17 15:20:44 +0400

    Change tests to only take 1 coordinate

--- src/logic.test.js
+++ src/logic.test.js
@@ -6,13 +6,13 @@ beforeEach(() => {
   console.log(field);
 });
 
-test.skip("test sink function", () => {
-  let ship1 = new logic.ships("p", [field[1][1], field[1][2], field[1][3]], 3);
+test("test sink function", () => {
+  let ship1 = new logic.ships("p", field[1][1], 3);
   expect(ship1.sunk()).toBe("ship has sunk");
 });
 
 test.skip("test hit function", () => {
-  let ship1 = new logic.ships("p", [field[1][1], field[1][2], field[1][3]], 3);
+  let ship1 = new logic.ships("p", field[1][1], 3);
 
   expect(ship1.hit()).toBe(2);
 });
