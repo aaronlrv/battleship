82436034994ebaa13d4161cf00bdada14d4f298d
aaronlrv <aaronvitharanage@gmail.com>
2022-07-17 17:58:37 +0400

    Fix more math related issues

--- src/logic.js
+++ src/logic.js
@@ -41,7 +41,6 @@ class gameboard {
       ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10"],
       ["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10"],
       ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10"],
-      ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10"],
       ["d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10"],
       ["e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9", "e10"],
       ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10"],
@@ -73,7 +72,7 @@ class gameboard {
       /// places vertically
       for (let i = 0; i < length; i++) {
         let j = ans;
-        board[j][column] = variable;
+        board[j][column - 1] = variable;
         ans++;
       }
       // for every loop
@@ -84,7 +83,7 @@ class gameboard {
       for (let i = 0; i < length; i++) {
         /// places it horizontally
         let j = ans;
-        board[column][j] = variable;
+        board[column - 1][j] = variable;
         ans++;
       }
     } else {
@@ -111,7 +110,7 @@ class gameboard {
     console.log(finalRow);
     let ans = finalRow.charCodeAt() - 65;
     console.log("Answer:", ans);
-    let coord = gameboard[ans][column];
+    let coord = gameboard[ans][column - 1];
 
     console.log("Final COords:", coord);
     if (typeof coord !== "string") {
