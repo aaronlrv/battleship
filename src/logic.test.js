138a8e7f5b28ed22fb4e5dcb4c72c07c322dc40d
aaronlrv <aaronvitharanage@gmail.com>
2022-07-17 11:18:51 +0400

    Fix vertical test to adapt to new gameboard class

--- src/logic.test.js
+++ src/logic.test.js
@@ -21,8 +21,6 @@ test("test gameboard function horizontal", () => {
   let gameboard = new logic.gameboard(field[1][1], "poo", 3, "horizontal");
   gameboard.gameboardPosition();
 
-  console.log(field);
-
   expect(field).toStrictEqual([
     ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10"],
     ["b1", "poo", "poo", "poo", "b5", "b6", "b7", "b8", "b9", "b10"],
@@ -39,16 +37,68 @@ test("test gameboard function horizontal", () => {
 });
 
 test("test gameboard function vertical", () => {
-  let gameboard = new logic.gameboard(field[2][3], "poo", 3, "vertical");
-  gameboard.gameboardPosition();
+  let ship = new logic.ships("ship", logic.field[1][1], 3, "vertical");
+  let variable = ship;
 
-  console.log(logic.field);
+  let gameboard = new logic.gameboard();
+  gameboard.gameboardPosition(ship, gameboard);
 
-  expect(field).toStrictEqual([
+  expect(gameboard.gameboardPosition(ship, gameboard)).toEqual([
     ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10"],
-    ["b1", "poo", "poo", "poo", "b5", "b6", "b7", "b8", "b9", "b10"],
-    ["c1", "c2", "c3", "poo", "c5", "c6", "c7", "c8", "c9", "c10"],
-    ["c1", "c2", "c3", "poo", "c5", "c6", "c7", "c8", "c9", "c10"],
+    [
+      "b1",
+      {
+        name: "ship",
+        start: "b2",
+        length: 3,
+        hp: 3,
+        orientation: "vertical",
+      },
+      "b3",
+      "b4",
+      "b5",
+      "b6",
+      "b7",
+      "b8",
+      "b9",
+      "b10",
+    ],
+    [
+      "c1",
+      {
+        name: "ship",
+        start: "b2",
+        length: 3,
+        hp: 3,
+        orientation: "vertical",
+      },
+      "c3",
+      "c4",
+      "c5",
+      "c6",
+      "c7",
+      "c8",
+      "c9",
+      "c10",
+    ],
+    [
+      "c1",
+      {
+        name: "ship",
+        start: "b2",
+        length: 3,
+        hp: 3,
+        orientation: "vertical",
+      },
+      "c3",
+      "c4",
+      "c5",
+      "c6",
+      "c7",
+      "c8",
+      "c9",
+      "c10",
+    ],
     ["d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10"],
     ["e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9", "e10"],
     ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10"],
