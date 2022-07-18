fe4f376dd33ace606d91845a0c6fc5161508d9e5
aaronlrv <aaronvitharanage@gmail.com>
2022-07-18 17:16:52 +0400

    Fix bug where objects wouldnt place

--- src/visual.js
+++ src/visual.js
@@ -4,19 +4,35 @@ let computerBoard = document.querySelector(".computerArray");
 function displayBoard(player, computer) {
   player.forEach((element) => {
     element.forEach((i) => {
-      let div = document.createElement("div");
-      div.classList.add(i);
-      div.textContent = i;
-      playerBoard.append(div);
+      if (typeof i !== "string") {
+        let name = i.name;
+        let div = document.createElement("div");
+        div.classList.add(name);
+        div.textContent = name;
+        playerBoard.append(div);
+      } else {
+        let div = document.createElement("div");
+        div.classList.add(i);
+        div.textContent = i;
+        playerBoard.append(div);
+      }
     });
   });
 
-  computer.forEach((i) => {
-    i.forEach((j) => {
-      let div = document.createElement("div");
-      div.classList.add("j");
-      div.textContent = j;
-      computerBoard.append(div);
+  computer.forEach((element) => {
+    element.forEach((i) => {
+      if (typeof i !== "string") {
+        let name = i.name;
+        let div = document.createElement("div");
+        div.classList.add(name);
+        div.textContent = name;
+        computerBoard.append(div);
+      } else {
+        let div = document.createElement("div");
+        div.classList.add(i);
+        div.textContent = i;
+        computerBoard.append(div);
+      }
     });
   });
 }
